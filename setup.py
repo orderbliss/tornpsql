ddfac999e825168e513f64131f046191bbbe882d
stevepeak <steve@stevepeak.net>
2014-02-18 08:34:52 -0500

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tornpsql',
-      version='0.1.0',
+      version='0.1.1',
       description="PostgreSQL handler for Tornado Web",
       long_description="",
       classifiers=["Development Status :: 5 - Production/Stable",
@@ -17,5 +17,5 @@ setup(name='tornpsql',
       packages=['tornpsql'],
       include_package_data=True,
       zip_safe=True,
-      install_requires=["psycopg2==2.5.1"],
+      install_requires=["psycopg2==2.5.2"],
       entry_points="")
