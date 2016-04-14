35a2e9d84c1c37e64caa3f3364d6c5bff61d287a
Steve Peak <steve@stevepeak.net>
2016-04-14 08:19:40 -0400

    v2.0.0a3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='tornpsql',
-      version='2.0.0a2',
+      version='2.0.0a3',
       description="PostgreSQL handler for Tornado Web",
       long_description="",
       classifiers=["Development Status :: 5 - Production/Stable",
